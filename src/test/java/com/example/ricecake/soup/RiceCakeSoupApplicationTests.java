fcb8174c0716544b99fb9c97b233183326b64185
SuperH0ng <ghdtmdgus100@gmail.com>
2022-12-27 00:05:53 +0900

    feat : entity

---
+++ src/test/java/com/example/ricecake/soup/RiceCakeSoupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ricecake.soup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiceCakeSoupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
