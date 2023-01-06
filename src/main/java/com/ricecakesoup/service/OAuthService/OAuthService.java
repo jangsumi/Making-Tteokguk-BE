2bfa5584ebb81a4a773874ea779010c5f18a40b9
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-01-07 05:38:00 +0900

    feat : Access Token 발급받는 코드 추가

--- src/main/java/com/ricecakesoup/service/OAuthService/OAuthService.java
+++ src/main/java/com/ricecakesoup/service/OAuthService/OAuthService.java
@@ -1,10 +1,11 @@
 package com.ricecakesoup.service.OAuthService;
 
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -12,7 +13,7 @@ import java.net.URL;
 
 @Service
 public class OAuthService {
-    public String getKakaoAccessToken (String code){
+    public String getKakaoAccessToken(String code) {
         String access_Token = "";
         String refresh_Token = "";
         String reqURL = "https://kauth.kakao.com/oauth/token";
@@ -30,13 +31,46 @@ public class OAuthService {
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
             sb.append("&client_id=01e20c4d528a4d85430b59e9a5a1c0a3"); //TODO REST_API KEY
+            sb.append("&redirect_uri=https://makingtteokguk.netlify.app/user/kakao/callback"); // TODO 인가코드 받은 redirect_uri 입력
+            sb.append("&code=" + code);
+            bw.write(sb.toString());
+            bw.flush();
+
+            //결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            access_Token = element.getAsJsonObject().get("access_token").getAsString();
+            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
+
+            System.out.println("access_token : " + access_Token);
+            System.out.println("refresh_token : " + refresh_Token);
+
+            br.close();
+            bw.close();
 
 
-    } catch (ProtocolException e) {
+        } catch (ProtocolException e) {
             throw new RuntimeException(e);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return access_Token;
     }
+}
