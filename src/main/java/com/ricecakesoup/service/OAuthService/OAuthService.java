053d8ad35f668ee113493a597647009c26283ad6
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-01-07 03:36:58 +0900

    feat : kakaologin 연동중

---
+++ src/main/java/com/ricecakesoup/service/OAuthService/OAuthService.java
@@ -0,0 +1,42 @@
+package com.ricecakesoup.service.OAuthService;
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+@Service
+public class OAuthService {
+    public String getKakaoAccessToken (String code){
+        String access_Token = "";
+        String refresh_Token = "";
+        String reqURL = "https://kauth.kakao.com/oauth/token";
+
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            //POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            //POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=01e20c4d528a4d85430b59e9a5a1c0a3"); //TODO REST_API KEY
+
+
+    } catch (ProtocolException e) {
+            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
