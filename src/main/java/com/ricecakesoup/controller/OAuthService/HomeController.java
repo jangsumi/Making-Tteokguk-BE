837b5d1f60c8df5075a002d344272e5fc6e8af0d
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-01-08 04:30:19 +0900

    feat: access token 관련 controller 작성

--- src/main/java/com/ricecakesoup/controller/OAuthService/HomeController.java
+++ src/main/java/com/ricecakesoup/controller/OAuthService/HomeController.java
@@ -1,4 +1,26 @@
 package com.ricecakesoup.controller.OAuthService;
 
+import com.ricecakesoup.service.OAuthService.OAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HomeController {
+    @Autowired
+    private OAuthService kakao;
+
+    @RequestMapping(value="/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value="/user/kakao/callback")
+    public String login(@RequestParam("code") String code) {
+        String access_Token = kakao.getKakaoAccessToken(code);
+        System.out.println("controller access_token : " + access_Token);
+        return "index";
+    }
 }
+
