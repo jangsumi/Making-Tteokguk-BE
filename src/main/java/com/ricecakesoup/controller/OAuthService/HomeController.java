fb5cd7b47dc0ffff123503d63397119f66ae0ca8
Eun-sun-Lee <eunsun2080@kookmin.ac.kr>
2023-01-08 04:37:39 +0900

    feat: token으로 userInfo 정보 parsing 코드 추가

--- src/main/java/com/ricecakesoup/controller/OAuthService/HomeController.java
+++ src/main/java/com/ricecakesoup/controller/OAuthService/HomeController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+
 @Controller
 public class HomeController {
     @Autowired
@@ -17,9 +20,16 @@ public class HomeController {
     }
 
     @RequestMapping(value="/user/kakao/callback")
-    public String login(@RequestParam("code") String code) {
+    public String login(@RequestParam("code") String code, HttpSession session) {
         String access_Token = kakao.getKakaoAccessToken(code);
-        System.out.println("controller access_token : " + access_Token);
+        HashMap<String, Object> userInfo = kakao.getUserInfo(access_Token);
+        System.out.println("login Controller : " + userInfo);
+
+        //    클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
+        if (userInfo.get("email") != null) {
+            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("access_Token", access_Token);
+        }
         return "index";
     }
 }
--- src/main/java/com/ricecakesoup/service/OAuthService/OAuthService.java
+++ src/main/java/com/ricecakesoup/service/OAuthService/OAuthService.java
@@ -1,8 +1,8 @@
 package com.ricecakesoup.service.OAuthService;
 
-import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -10,6 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.HashMap;
 
 @Service
 public class OAuthService {
@@ -73,6 +74,52 @@ public class OAuthService {
             throw new RuntimeException(e);
         }
         return access_Token;
+    }
+
+    public HashMap<String, Object> getUserInfo (String access_Token) {
+
+        //    요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+        HashMap<String, Object> userInfo = new HashMap<>();
+        String reqURL = "https://kapi.kakao.com/v2/user/me";
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+            //    요청에 필요한 Header에 포함될 내용
+            conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+            int responseCode = conn.getResponseCode();
+            System.out.println("responseCode : " + responseCode);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            System.out.println("response body : " + result);
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+            JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+            String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+            String email = kakao_account.getAsJsonObject().get("email").getAsString();
+
+            userInfo.put("nickname", nickname);
+            userInfo.put("email", email);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return userInfo;
     }
 }
 
